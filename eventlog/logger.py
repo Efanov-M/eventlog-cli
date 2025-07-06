7c37ec52d6845a43cebeec966f564ddfcd335c1f
Efanov-M <m.efanov@bk.ru>
2025-07-06 13:26:00 +0300

    Добвлена возможность поиска лога по слову в сообщении

--- eventlog/logger.py
+++ eventlog/logger.py
@@ -15,8 +15,5 @@ def log_event(event_type, level, message):
     if os.path.isfile("eventlog.log"):
         with open("eventlog.log", "a") as file:
             log_time = datetime.now()
-            log_str = f"{log_time.strftime("%d.%m.%Y %H:%M:%S")} {event_type} {level} {message}"
-            file.write(f"{log_str}/n")
-    else:
-        with open("eventlog.log", "x") as file:
-            return
+            log_str = f"{log_time.strftime('%d.%m.%Y %H:%M:%S')} {event_type} {level} {message}"
+            file.write(f" {log_str} \n")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
+import os
 import sys
+from datetime import datetime
 
 import eventlog.logger
 
@@ -8,17 +10,34 @@ parser = argparse.ArgumentParser(description="Утилита для ведени
 # создаем ключи
 
 
-parser.add_argument("--type", type=str, help="тип события ( SYSTEM, USER, NETWORK)")
-parser.add_argument("--level", type=str, help="Уровень важности: INFO, WARNING, ERROR")
+parser.add_argument(
+    "--type",
+    choices=["SYSTEM", "USER", "APP"],
+    metavar="TYPE",
+    help="Тип события: SYSTEM | USER | APP",
+)
+
+parser.add_argument(
+    "--level",
+    choices=["INFO", "WARNING", "ERROR"],
+    metavar="LEVEL",
+    help="Уровень важности: INFO | WARNING | ERROR",
+)
 parser.add_argument("--message", type=str, help="Основной текст события")
+parser.add_argument("--date", type=str, help="дата  события")
+parser.add_argument(
+    "----keyword",
+    type=str,
+    help="позволяет фильтровать события по вхождению слова или части текста в поле сообщения ",
+)
 
 # создаем группу
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument(
     "--add", action="store_true", help="говорит, что нужно добавить запись"
 )
-group.add_argument("--show", action="store_true", help="")
-
+group.add_argument("--show", action="store_true", help="показ журнала логов")
+group.add_argument("--clear", action="store_true", help="очиска журнала")
 
 args = parser.parse_args()
 
@@ -26,8 +45,33 @@ args = parser.parse_args()
 
 if args.show:
     logs = eventlog.logger.read_logs()
+    formatted_date = None
+    if args.date:
+
+        try:
+            # Преобразуем введённую дату к нужному формату
+            user_date = datetime.strptime(args.date, "%Y-%m-%d")
+            formatted_date = user_date.strftime("%d.%m.%Y")  # формат логов
+        except ValueError:
+            print("❌ Некорректный формат даты. Используй YYYY-MM-DD.")
+            sys.exit(1)
+
     for line in logs:
-        print(line.strip())
+        parts = line.strip().split(" ", 4)
+        message_logs = parts[4] if len(parts) == 5 else ""
+        show_line = True
+
+        if args.type and args.type not in line:
+            show_line = False
+        if args.level and args.level not in line:
+            show_line = False
+        if args.date and formatted_date and not line.startswith(formatted_date):
+            show_line = False
+        if args.keyword and args.keyword not in message_logs:
+            show_line = False
+
+        if show_line:
+            print(line.strip())
 
 
 if args.add:
@@ -36,3 +80,17 @@ if args.add:
     else:
         print("❗️Убедитесь, что вы указали --type, --level и --message")
         sys.exit(1)
+
+
+if args.clear:
+    if os.path.isfile("eventlog.log"):
+        user_in = input("Вы действительно хотите удалить все записи? [y/N]")
+        user_in = user_in.lower()
+        if user_in == "y":
+            with open("eventlog.log", "w") as file:
+                print(" ✅ Журнал очищен.")
+        else:
+            print("⚠️ Очистка отменена.")
+            sys.exit(0)
+    else:
+        print("⚠️ Журнал ещё не создан.")
